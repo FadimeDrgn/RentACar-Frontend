74d887be739dfca0fef007bac7a58b53295ad55f
Fadime Durğun <durgunfadime@gmail.com>
2021-04-01 16:28:24 +0300

    03.31.2021

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from 'src/app/models/car/car';
-import { CarResponseModel } from 'src/app/models/car/carResponseModel';
 import { CarService } from 'src/app/services/car/car.service';
 
 @Component({
